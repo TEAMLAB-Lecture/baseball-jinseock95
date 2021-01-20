c3ef8354fdaa2590bb7bda1d6eb0797d99a902ef
jinseok <jinseock95@gmail.com>
2021-01-20 10:40:43 +0900

    del zero termination condition

--- baseball_game.py
+++ baseball_game.py
@@ -306,7 +306,7 @@ def main():
     while game:
         user_input = input('Input guess number : ')
 
-        if user_input == '0': break # 종료 조건: 사용자가 게임 중 0를 입력하게 되면 종료
+        # if user_input == '0': break # 종료 조건: 사용자가 게임 중 0를 입력하게 되면 종료
 
         if is_validated_number(user_input): # 사용자가 입력한 number 체크, 3가지 조건
             strikes, balls = get_strikes_or_ball(user_input, random_number)
